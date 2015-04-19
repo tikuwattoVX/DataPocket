79e3f25d7ebc53114bf403104709c6539719f87a
masaki sakamoto <sakamotooribettii@gmail.com>
2015-04-19 13:33:16 +0900

    最新

--- app/java/activity/DetailActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.sample.datapocket.activity;
-
-/**
- * Created by masakisakamoto on 2015/04/05.
- */
-public class DetailActivity {
-}
--- app/java/item/DataItem.java
+++
@@ -1,7 +0,0 @@
-package com.example.sample.datapocket.item;
-
-/**
- * Created by masakisakamoto on 2015/04/06.
- */
-public class DataItem {
-}
